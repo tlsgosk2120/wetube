1dd3ffc3d60091ba07146d82d6fd12e713025751
Haena <2139713-tlsgosksla@users.noreply.gitlab.com>
2021-05-18 11:20:37 +0900

    #3.7 Express Core: Middlewares 까지

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("Hi!");
+// const express = require("express");
+import "core-js";
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+// import bodyParser from "body-parser"; // bodyParser 모듈이 express 모듈에 내장되기 때문에 별도로 설치하지 않아도 된다
+
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send("Hello from home");
+
+const handleProfile = (req, res) => res.send("You are on my Profile");
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.unsubscribe(helmet());
+app.use(morgan("dev"));
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
+// app.get("/", function (req, res) {
+//   res.send("hello world");
+// });
