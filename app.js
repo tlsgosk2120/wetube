3735b69aa148c6d1d436a85e6135908c3e0da858
Haena <2139713-tlsgosksla@users.noreply.gitlab.com>
2021-05-21 10:40:43 +0900

    #3.16 Local Variables in Pug

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 // import bodyParser from "body-parser"; // bodyParser 모듈이 express 모듈에 내장되기 때문에 별도로 설치하지 않아도 된다
+import { localMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -11,12 +12,13 @@ import routers from "./routers";
 
 const app = express();
 
+app.unsubscribe(helmet());
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.unsubscribe(helmet());
 app.use(morgan("dev"));
+app.use(localMiddleware);
 
 app.use(routers.home, globalRouter);
 app.use(routers.users, userRouter);
