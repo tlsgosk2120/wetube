3466e97e1fb032927cf9809f469168235a68cdeb
Haena <2139713-tlsgosksla@users.noreply.gitlab.com>
2021-05-21 16:03:17 +0900

    #3.20 Change Profile HTML

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -10,8 +10,8 @@ import routes from "../routers";
 const videoRouter = express.Router();
 
 videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.videoDetail, videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
+videoRouter.get(routes.videoDetail, videoDetail);
 
 export default videoRouter;
