6a806d1896a8925eda64127b0fb5a65a4528313f
Haena <2139713-tlsgosksla@users.noreply.gitlab.com>
2021-06-04 15:02:18 +0900

    #6.6 More Practice part Two

--- src/db.js
+++ src/db.js
@@ -6,12 +6,6 @@ export const videos = [
     createdAt: "2 minutes ago",
     views: 59,
     id: 1,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
-    creator: {
-      id: 121212,
-      name: "Haena",
-      email: "hana@gmail.com",
-    },
   },
   {
     title: "Second Video",
@@ -20,12 +14,6 @@ export const videos = [
     createdAt: "2 minutes ago",
     views: 1,
     id: 2,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
-    creator: {
-      id: 121212,
-      name: "Haena",
-      email: "hana@gmail.com",
-    },
   },
   {
     title: "Third Video",
@@ -34,11 +22,5 @@ export const videos = [
     createdAt: "2 minutes ago",
     views: 59,
     id: 3,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
-    creator: {
-      id: 121212,
-      name: "Haena",
-      email: "hana@gmail.com",
-    },
   },
 ];
