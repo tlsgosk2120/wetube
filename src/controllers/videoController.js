974aadbf6e6c9ce21b34fe8554ffa69282c70aba
Haena <2139713-tlsgosksla@users.noreply.gitlab.com>
2021-06-16 12:29:54 +0900

    #6.16 Creating a Video part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,46 +3,45 @@ import Video from "../models/Video";
 // Video.find({}, (error, videos) => {});
 
 export const home = async (req, res) => {
-    const videos = await Video.find({});
-    return res.render("home", { pageTitle: "Home", videos });
+  const videos = await Video.find({});
+  return res.render("home", { pageTitle: "Home", videos });
 };
 
 export const watch = (req, res) => {
-    const { id } = req.params;
-    return res.render("watch", { pageTitle: `Watching` });
+  const { id } = req.params;
+  return res.render("watch", { pageTitle: `Watching` });
 };
 
 export const getEdit = (req, res) => {
-    const { id } = req.params;
-    return res.render("edit", { pageTitle: `Editing` });
+  const { id } = req.params;
+  return res.render("edit", { pageTitle: `Editing` });
 };
 
 export const postEdit = (req, res) => {
-    const { id } = req.params;
-    const { title } = req.body;
-    // const video = videos[id - 1];
-    // video.title = title;
-    return res.redirect(`/videos/${id}`);
+  const { id } = req.params;
+  const { title } = req.body;
+  // const video = videos[id - 1];
+  // video.title = title;
+  return res.redirect(`/videos/${id}`);
 };
 
 export const getUpload = (req, res) => {
-    return res.render("upload", { pageTitle: "Upload Video" });
+  return res.render("upload", { pageTitle: "Upload Video" });
 };
 
-export const postUpload = (req, res) => {
-    // here we will add a video to the videos array.
-    const { title, description, hashtags } = req.body;
-    const video = new Video({
-        title,
-        description,
-        createAt: Date.now(),
-        hashtags: hashtags.split(",").map((word) => `#${word}`),
-        meta: {
-            views: 0,
-            rating: 0,
-        },
-    });
-    return res.redirect(`/`);
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  await Video.create({
+    title,
+    description,
+    createAt: Date.now(),
+    hashtags: hashtags.split(",").map((word) => `#${word}`),
+    meta: {
+      views: 0,
+      rating: 0,
+    },
+  });
+  return res.redirect(`/`);
 };
 
 // export const search = (req, res) => {
