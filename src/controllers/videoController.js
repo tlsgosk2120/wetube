66ef5f29401126b7522a3f255f15489c4e42da84
Haena <2139713-tlsgosksla@users.noreply.gitlab.com>
2021-06-24 11:48:50 +0900

    #6.27 Search part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -69,7 +69,13 @@ export const deleteVideo = async (req, res) => {
 
 export const search = async (req, res) => {
   const { keyword } = req.query;
+  let videos = [];
   if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(keyword, "i"),
+      },
+    });
   }
-  return res.render("search", { pageTitle: "Search" });
+  return res.render("search", { pageTitle: "Search", videos });
 };
