ffe751285b157be3e0b799b2bd8696347e101e5f
tlsgosk2120 <tlsgosksla@gmail.com>
2021-06-28 22:01:00 +0900

    #7.5 Login part One

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -18,21 +18,38 @@ export const postJoin = async (req, res) => {
             errorMessage: "This usernam/emaile is already taken.",
         });
     }
-    await User.create({
-        name,
-        username,
-        email,
-        password,
-        location,
-    });
-    res.redirect("/login");
+    try {
+        await User.create({
+            name,
+            username,
+            email,
+            password,
+            location,
+        });
+        return res.redirect("/login");
+    } catch (error) {
+        return res.status(400).render("join", {
+            pageTitle: "Join",
+            errorMessage: error._message,
+        });
+    }
+};
+export const getLogin = (req, res) => {
+    return res.render("login", { pageTitle: "Login" });
+};
+export const postLogin = async (req, res) => {
+    const { username, password } = req.body;
+    const exists = await User.exists({ username });
+    if (!exists) {
+        return res.status(400).render("login", { pageTitle: "Login", errorMessage: "An account with this username does not exists." });
+    }
+    // check if password correct
+    return res.render("login", { pageTitle: "Login" });
 };
 
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit User" });
 
 export const remove = (req, res) => res.render("removeProfile", { pageTitle: "Remove User" });
-
-export const login = (req, res) => res.render("login", { pageTitle: "Login" });
 
 export const logout = (req, res) => res.render("logout", { pageTitle: "Logout" });
 
--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getJoin, postJoin, login } from "../controllers/userController";
+import { getJoin, postJoin, getLogin, postLogin } from "../controllers/userController";
 import { search, home } from "../controllers/videoController";
 import routes from "../routers";
 
@@ -7,7 +7,7 @@ const rootRouter = express.Router();
 
 rootRouter.get(routes.home, home);
 rootRouter.route(routes.join).get(getJoin).post(postJoin);
-rootRouter.get(routes.login, login);
+rootRouter.route(routes.login).get(getLogin).post(postLogin);
 rootRouter.get(routes.search, search);
 
 export default rootRouter;
