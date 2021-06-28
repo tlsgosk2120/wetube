3edd63299a0fa1880a036c317878944794f50011
tlsgosk2120 <tlsgosksla@gmail.com>
2021-06-28 21:27:39 +0900

    #7.4 Status Codes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,45 +2,41 @@ import User from "../models/User";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
-  console.log(req.body);
-  const { name, username, email, password, password2, location } = req.body;
-  const pageTitle = "Join";
-  if (password !== password2) {
-    return res.render("join", {
-      pageTitle,
-      errorMessage: "Password confirmation does not match.",
+    console.log(req.body);
+    const { name, username, email, password, password2, location } = req.body;
+    const pageTitle = "Join";
+    if (password !== password2) {
+        return res.status(404).render("join", {
+            pageTitle,
+            errorMessage: "Password confirmation does not match.",
+        });
+    }
+    const exists = await User.exists({ $or: [{ username }, { email }] });
+    if (exists) {
+        return res.status(404).render("join", {
+            pageTitle,
+            errorMessage: "This usernam/emaile is already taken.",
+        });
+    }
+    await User.create({
+        name,
+        username,
+        email,
+        password,
+        location,
     });
-  }
-  const exists = await User.exists({ $or: [{ username }, { email }] });
-  if (exists) {
-    return res.render("join", {
-      pageTitle,
-      errorMessage: "This usernam/emaile is already taken.",
-    });
-  }
-  await User.create({
-    name,
-    username,
-    email,
-    password,
-    location,
-  });
-  res.redirect("/login");
+    res.redirect("/login");
 };
 
-export const edit = (req, res) =>
-  res.render("edit", { pageTitle: "Edit User" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit User" });
 
-export const remove = (req, res) =>
-  res.render("removeProfile", { pageTitle: "Remove User" });
+export const remove = (req, res) => res.render("removeProfile", { pageTitle: "Remove User" });
 
 export const login = (req, res) => res.render("login", { pageTitle: "Login" });
 
-export const logout = (req, res) =>
-  res.render("logout", { pageTitle: "Logout" });
+export const logout = (req, res) => res.render("logout", { pageTitle: "Logout" });
 
-export const see = (req, res) =>
-  res.render("userDetail", { pageTitle: "See User" });
+export const see = (req, res) => res.render("userDetail", { pageTitle: "See User" });
 
 // export const changePassword = (req, res) =>
 //   res.render("changePassword", { pageTitle: "Change Password" });
