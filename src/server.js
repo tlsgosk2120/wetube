0c36bbde26460ef3f807dd9c6fbbe757ba5c2880
Haena <2139713-tlsgosksla@users.noreply.gitlab.com>
2021-06-29 12:40:15 +0900

    #7.8 Sessions and Cookies part Two

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import "core-js";
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 // import bodyParser from "body-parser"; // bodyParser 모듈이 express 모듈에 내장되기 때문에 별도로 설치하지 않아도 된다
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -13,11 +14,22 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
-// app.use(cookieParser());
-// app.use(express.json());
+
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.get("/add-one", (req, res, next) => {
+  req.session.potato += 1;
+  return res.send(`${req.session.id}\n${req.session.potato}`);
+});
 
 app.use("/", rootRouter);
-app.use("/users", userRouter);
 app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 export default app;
