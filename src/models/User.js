065ae7a84fc86141cd861dbbe9b8953c39ae7042
Haena <2139713-tlsgosksla@users.noreply.gitlab.com>
2021-06-30 18:19:04 +0900

    #7.21 Github Login part Six

--- src/models/User.js
+++ src/models/User.js
@@ -3,8 +3,9 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, require: true, unique: true },
+  socialOnly: { type: Boolean, default: false },
   username: { type: String, require: true, unique: true },
-  password: { type: String, require: true },
+  password: { type: String },
   name: { type: String, require: true },
   location: String,
 });
