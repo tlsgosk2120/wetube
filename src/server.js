b2578ee597b236c187edbfb0cfea85c72fc9512f
Haena <2139713-tlsgosksla@users.noreply.gitlab.com>
2021-06-30 15:33:22 +0900

    #7.14 Expiration and secrets

--- src/server.js
+++ src/server.js
@@ -19,10 +19,13 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   session({
-    secret: "Hello!",
+    secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube" }),
+    // cookie: {
+    //   maxAge: 20000,
+    // },
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
 
