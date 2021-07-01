f8f3f53d8780c24976aef5f5e9a26c0978c5edb9
Haena <2139713-tlsgosksla@users.noreply.gitlab.com>
2021-07-01 17:04:10 +0900

    #8.2 Edit Profile POST part One

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -7,13 +7,22 @@ import {
   postUpload,
   watch,
 } from "../controllers/videoController";
+import { protectorMiddleware } from "../middlewares";
 import routes from "../routers";
 
 const videoRouter = express.Router();
 
 videoRouter.get(routes.watch, watch);
-videoRouter.route(routes.editVideo).get(getEdit).post(postEdit);
-videoRouter.route(routes.upload).get(getUpload).post(postUpload);
-videoRouter.route(routes.deleteVideo).get(deleteVideo);
+videoRouter
+  .route(routes.editVideo)
+  .all(protectorMiddleware)
+  .get(getEdit)
+  .post(postEdit);
+videoRouter
+  .route(routes.upload)
+  .all(protectorMiddleware)
+  .get(getUpload)
+  .post(postUpload);
+videoRouter.route(routes.deleteVideo).all(protectorMiddleware).get(deleteVideo);
 
 export default videoRouter;
