b4df1437f0ba4e530dbc9756acc7ae46a6426f27
Haena <2139713-tlsgosksla@users.noreply.gitlab.com>
2021-07-01 16:52:53 +0900

    #8.1 Protector and Public Middlewares

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,13 +1,27 @@
 import express from "express";
-import { getJoin, postJoin, getLogin, postLogin } from "../controllers/userController";
+import {
+  getJoin,
+  postJoin,
+  getLogin,
+  postLogin,
+} from "../controllers/userController";
 import { search, home } from "../controllers/videoController";
+import { publicOnlyMiddleware } from "../middlewares";
 import routes from "../routers";
 
 const rootRouter = express.Router();
 
 rootRouter.get(routes.home, home);
-rootRouter.route(routes.join).get(getJoin).post(postJoin);
-rootRouter.route(routes.login).get(getLogin).post(postLogin);
+rootRouter
+  .route(routes.join)
+  .all(publicOnlyMiddleware)
+  .get(getJoin)
+  .post(postJoin);
+rootRouter
+  .route(routes.login)
+  .all(publicOnlyMiddleware)
+  .get(getLogin)
+  .post(postLogin);
 rootRouter.get(routes.search, search);
 
 export default rootRouter;
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -8,14 +8,19 @@ import {
   startGithubLogin,
   finishGithubLogin,
 } from "../controllers/userController";
+import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 import routes from "../routers";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.logout, logout);
-userRouter.route(routes.editProfile).get(getEdit).post(postEdit);
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin);
+userRouter.get(routes.logout, protectorMiddleware, logout);
+userRouter
+  .route(routes.editProfile)
+  .all(protectorMiddleware)
+  .get(getEdit)
+  .post(postEdit);
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get(routes.see, see);
 // userRouter.get(routes.changePassword, changePassword);
 
