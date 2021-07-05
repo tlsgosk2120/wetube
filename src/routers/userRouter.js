a374a53b8bd30ac85639001a9920ed43ddc2469e
tlsgosk2120 <tlsgosksla@gmail.com>
2021-07-05 21:23:05 +0900

    #8.6 File Uploads part One

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,30 +1,13 @@
 import express from "express";
-import {
-  getEdit,
-  postEdit,
-  see,
-  logout,
-  startGithubLogin,
-  finishGithubLogin,
-  getChangePassword,
-  postChangePassword,
-} from "../controllers/userController";
-import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
+import { getEdit, postEdit, see, logout, startGithubLogin, finishGithubLogin, getChangePassword, postChangePassword } from "../controllers/userController";
+import { protectorMiddleware, publicOnlyMiddleware, uploadFiles } from "../middlewares";
 import routes from "../routers";
 
 const userRouter = express.Router();
 
 userRouter.get(routes.logout, protectorMiddleware, logout);
-userRouter
-  .route(routes.editProfile)
-  .all(protectorMiddleware)
-  .get(getEdit)
-  .post(postEdit);
-userRouter
-  .route(routes.changePassword)
-  .all(protectorMiddleware)
-  .get(getChangePassword)
-  .post(postChangePassword);
+userRouter.route(routes.editProfile).all(protectorMiddleware).get(getEdit).post(uploadFiles.single("avatar"), postEdit);
+userRouter.route(routes.changePassword).all(protectorMiddleware).get(getChangePassword).post(postChangePassword);
 userRouter.get(routes.see, see);
 userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
