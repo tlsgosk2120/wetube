9dfcd82284ecaa5c03a1b27866719b87ae2e104e
Haena <2139713-tlsgosksla@users.noreply.gitlab.com>
2021-07-12 17:19:44 +0900

    #11.2 Mute and Unmute

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,30 +2,22 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
 
 // console.log(play, mute, time, volume);
 
 const handlePlayClick = (e) => {
   // if the video is playing, pause it
-  if (video.paused) {
-    playBtn.innerText = "Pause";
-    video.play();
-  } else {
-    playBtn.innerText = "Play";
-    video.pause();
-  }
   // else play the video.
+  playBtn.innerText = video.paused ? video.play() : video.pause();
+  playBtn.innerText = video.paused ? "Play" : "Pause";
 };
 
-const handleMuteClick = (e) => {
-  //
+const handleMute = (e) => {
+  video.muted = video.muted ? false : true;
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : 0.5;
 };
-
-const handlePause = () => (playBtn.innerText = "Play");
-const handlePlay = () => (playBtn.innerText = "Pause");
 
 playBtn.addEventListener("click", handlePlayClick);
-muteBtn.addEventListener("click", handleMuteClick);
-video.addEventListener("pause", handlePause);
-video.addEventListener("play", handlePlay);
+muteBtn.addEventListener("click", handleMute);
