9049c528707e5332182f3c7bd246f542eec8ebf8
Haena <2139713-tlsgosksla@users.noreply.gitlab.com>
2021-07-12 17:07:47 +0900

    #11.1 Play Pause

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,31 @@
-console.log("Video Player");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+// console.log(play, mute, time, volume);
+
+const handlePlayClick = (e) => {
+  // if the video is playing, pause it
+  if (video.paused) {
+    playBtn.innerText = "Pause";
+    video.play();
+  } else {
+    playBtn.innerText = "Play";
+    video.pause();
+  }
+  // else play the video.
+};
+
+const handleMuteClick = (e) => {
+  //
+};
+
+const handlePause = () => (playBtn.innerText = "Play");
+const handlePlay = () => (playBtn.innerText = "Pause");
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMuteClick);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
