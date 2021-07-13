3efb709585cd4891f85a343022a93ae926e976ee
tlsgosk2120 <tlsgosksla@gmail.com>
2021-07-13 22:36:29 +0900

    #13.2 Recording Video

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,13 +1,36 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
-    audio: false,
-    video: true,
-  });
-  video.srcObject = stream;
-  video.play();
+let stream;
+
+const handleStop = () => {
+    startBtn.innerText = "Start Recording";
+    startBtn.removeEventListener("click", handleStop);
+    startBtn.addEventListener("click", handleStart);
 };
+const handleStart = () => {
+    startBtn.innerText = "Stop Recording";
+    startBtn.removeEventListener("click", handleStart);
+    startBtn.addEventListener("click", handleStop);
+    const recorder = new MediaRecorder(stream);
+    recorder.ondataavailable = (e) => {
+        console.log(e.data);
+    };
+    recorder.start();
+    setTimeout(() => {
+        recorder.stop();
+    }, 10000);
+};
+
+const init = async () => {
+    stream = await navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: true,
+    });
+    video.srcObject = stream;
+    video.play();
+};
+
+init();
 
 startBtn.addEventListener("click", handleStart);
