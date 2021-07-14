1efdfb74fce25c93b7728c95cc5d9a08441b0da2
tlsgosk2120 <tlsgosksla@gmail.com>
2021-07-14 23:32:35 +0900

    #16.4 API Route part Two

--- src/server.js
+++ src/server.js
@@ -18,17 +18,18 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(
-  session({
-    secret: process.env.COOKIE_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    // cookie: {
-    //   maxAge: 20000,
-    // },
-    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
-  })
+    session({
+        secret: process.env.COOKIE_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        // cookie: {
+        //   maxAge: 20000,
+        // },
+        store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+    })
 );
 
 app.use(flash());
