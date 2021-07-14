c50c7c3e9d8c029a900e9a50a4ff249f49f13cc9
Haena <2139713-tlsgosksla@users.noreply.gitlab.com>
2021-07-14 11:41:04 +0900

    #14.1 Transcode Video

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,3 +1,6 @@
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+import { async } from "regenerator-runtime";
+
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
@@ -5,42 +8,48 @@ let stream;
 let recorder;
 let videoFile;
 
-const handleDownload = () => {
-    const a = document.createElement("a");
-    a.href = videoFile;
-    a.download = "MyRecording.webm";
-    document.body.appendChild(a);
-    a.click();
+const handleDownload = async () => {
+  const ffmpeg = createFFmpeg({ log: true });
+  await ffmpeg.load();
+
+  ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
+  await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
+
+  const a = document.createElement("a");
+  a.href = videoFile;
+  a.download = "MyRecording.webm";
+  document.body.appendChild(a);
+  a.click();
 };
 
 const handleStop = () => {
-    startBtn.innerText = "Download Recording";
-    startBtn.removeEventListener("click", handleStop);
-    startBtn.addEventListener("click", handleDownload);
-    recorder.stop();
+  startBtn.innerText = "Download Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleDownload);
+  recorder.stop();
 };
 const handleStart = () => {
-    startBtn.innerText = "Stop Recording";
-    startBtn.removeEventListener("click", handleStart);
-    startBtn.addEventListener("click", handleStop);
-    recorder = new MediaRecorder(stream);
-    recorder.ondataavailable = (event) => {
-        videoFile = URL.createObjectURL(event.data);
-        video.srcObject = null;
-        video.src = videoFile;
-        video.loop = true;
-        video.play();
-    };
-    recorder.start();
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+  recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (event) => {
+    videoFile = URL.createObjectURL(event.data);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
+  };
+  recorder.start();
 };
 
 const init = async () => {
-    stream = await navigator.mediaDevices.getUserMedia({
-        audio: false,
-        video: true,
-    });
-    video.srcObject = stream;
-    video.play();
+  stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: true,
+  });
+  video.srcObject = stream;
+  video.play();
 };
 
 init();
