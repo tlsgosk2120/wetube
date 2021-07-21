2aab4126eabfaf667ae3a9e74c9f9648385de11b
tlsgosk2120 <tlsgosksla@gmail.com>
2021-07-22 00:06:08 +0900

    #16.9 Recap and Challenge

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -5,6 +5,6 @@ const apiRouter = express.Router();
 
 apiRouter.post("/videos/:id([0-9a-f]{24})/view", registerView);
 apiRouter.post("/videos/:id([0-9a-f]{24})/comment", createComment);
-apiRouter.delete("/videos/comment/:id([0-9a-f]{24})", deleteComment);
+apiRouter.delete("/comments/:id([0-9a-f]{24})/delete", deleteComment);
 
 export default apiRouter;
