d2fbc0a906236bd436d6a57c06634c868cbb9e68
tlsgosk2120 <tlsgosksla@gmail.com>
2021-07-26 23:49:07 +0900

    comment btn css event finish

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,167 +1,193 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
-const deleteBtn = document.querySelectorAll(".comment__delete");
+const deleteBtns = document.querySelectorAll(".comment__delete");
 const videoComments = document.querySelector(".video__comments ul");
-const likesBtn = document.querySelectorAll(".fa-thumbs-up");
-const dislikesBtn = document.querySelectorAll(".fa-thumbs-down");
-const CommentMenuBtn = document.querySelectorAll(".comment__menu");
+const likesBtns = document.querySelectorAll(".fa-thumbs-up");
+const dislikesBtns = document.querySelectorAll(".fa-thumbs-down");
+const commentMenuBtns = document.querySelectorAll(".comment__menu");
+const commentMenus = document.querySelectorAll(".comment__menu_box");
+const lovesBtns = document.querySelectorAll(".comment__heart");
+const DELETE_BOX = "comment__delete";
 
 const addComment = (text, id) => {
-  const newComment = document.createElement("li");
-  const userIcon = document.createElement("div");
-  const icon = document.createElement("i");
-  const commentElse = document.createElement("div");
-  const elseDiv = document.createElement("div");
-  const nameSpan = document.createElement("span");
-  const createdAtSpan = document.createElement("span");
-  const comment = document.createElement("div");
-  const textBox = document.createElement("pre");
-  const commentIcons = document.createElement("div");
-  const likes = document.createElement("span");
-  const likesIcon = document.createElement("span");
-  const likesCount = document.createElement("span");
-  const dislike = document.createElement("span");
-  const dislikesIcon = document.createElement("span");
-  const dislikesCount = document.createElement("span");
-  const deleted = document.createElement("span");
-  const { name } = document.querySelector(".user__name").dataset;
-  newComment.dataset.id = id;
-  newComment.className = "video__comment";
-  userIcon.className = "comment__userIcon";
-  icon.className = "fas fa-comment";
-  commentElse.className = "comment__else";
-  createdAtSpan.className = "comment__createdAt";
-  textBox.className = "comment__text";
-  commentIcons.className = "comment__icons";
-  likes.className = "likes";
-  likesIcon.className = "far fa-thumbs-up";
-  dislike.className = "dislikes";
-  dislikesIcon.className = "far fa-thumbs-down";
-  deleted.className = "fas fa-trash comment__delete";
-  textBox.innerText = `${text}`;
-  nameSpan.innerText = `${name}`;
-  createdAtSpan.innerText =
-    "   ·  " +
-    new Date().toLocaleDateString("ko-kr", {
-      weekday: "long",
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    });
-  likesCount.innerText = `  0`;
-  dislikesCount.innerText = `  0`;
-  elseDiv.appendChild(nameSpan);
-  elseDiv.appendChild(createdAtSpan);
-  likes.appendChild(likesIcon);
-  likes.appendChild(likesCount);
-  dislike.appendChild(dislikesIcon);
-  dislike.appendChild(dislikesCount);
-  commentIcons.appendChild(likes);
-  commentIcons.appendChild(dislike);
-  commentIcons.appendChild(deleted);
-  comment.appendChild(textBox);
-  commentElse.appendChild(elseDiv);
-  commentElse.appendChild(comment);
-  commentElse.appendChild(commentIcons);
-  userIcon.appendChild(icon);
-  newComment.appendChild(userIcon);
-  newComment.appendChild(commentElse);
-  videoComments.prepend(newComment);
+    const newComment = document.createElement("li");
+    const userIcon = document.createElement("div");
+    const icon = document.createElement("i");
+    const commentElse = document.createElement("div");
+    const elseDiv = document.createElement("div");
+    const nameSpan = document.createElement("span");
+    const createdAtSpan = document.createElement("span");
+    const comment = document.createElement("div");
+    const textBox = document.createElement("pre");
+    const commentIcons = document.createElement("div");
+    const likes = document.createElement("span");
+    const likesIcon = document.createElement("span");
+    const likesCount = document.createElement("span");
+    const dislike = document.createElement("span");
+    const dislikesIcon = document.createElement("span");
+    const dislikesCount = document.createElement("span");
+    const deleted = document.createElement("span");
+    const { name } = document.querySelector(".user__name").dataset;
+    newComment.dataset.id = id;
+    newComment.className = "video__comment";
+    userIcon.className = "comment__userIcon";
+    icon.className = "fas fa-comment";
+    commentElse.className = "comment__else";
+    createdAtSpan.className = "comment__createdAt";
+    textBox.className = "comment__text";
+    commentIcons.className = "comment__icons";
+    likes.className = "likes";
+    likesIcon.className = "far fa-thumbs-up";
+    dislike.className = "dislikes";
+    dislikesIcon.className = "far fa-thumbs-down";
+    deleted.className = "fas fa-trash comment__delete_btn";
+    textBox.innerText = `${text}`;
+    nameSpan.innerText = `${name}`;
+    createdAtSpan.innerText =
+        "   ·  " +
+        new Date().toLocaleDateString("ko-kr", {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+        });
+    likesCount.innerText = `  0`;
+    dislikesCount.innerText = `  0`;
+    elseDiv.appendChild(nameSpan);
+    elseDiv.appendChild(createdAtSpan);
+    likes.appendChild(likesIcon);
+    likes.appendChild(likesCount);
+    dislike.appendChild(dislikesIcon);
+    dislike.appendChild(dislikesCount);
+    commentIcons.appendChild(likes);
+    commentIcons.appendChild(dislike);
+    commentIcons.appendChild(deleted);
+    comment.appendChild(textBox);
+    commentElse.appendChild(elseDiv);
+    commentElse.appendChild(comment);
+    commentElse.appendChild(commentIcons);
+    userIcon.appendChild(icon);
+    newComment.appendChild(userIcon);
+    newComment.appendChild(commentElse);
+    videoComments.prepend(newComment);
 
-  deleted.addEventListener("click", handleDeleteComment);
+    deleted.addEventListener("click", handleDeleteComment);
 };
 
 const deleteComment = (target) => {
-  const videoComment = target.parentNode.parentNode.parentNode;
-  videoComments.removeChild(videoComment);
+    let videoComment;
+    if (target.className === DELETE_BOX) {
+        videoComment = target.parentNode.parentNode.parentNode.parentNode;
+    } else {
+        videoComment = target.parentNode.parentNode.parentNode.parentNode.parentNode;
+    }
+    videoComments.removeChild(videoComment);
 };
 
 const handleSubmit = async (event) => {
-  event.preventDefault();
-  const textarea = form.querySelector("textarea");
-  const text = textarea.value;
-  const videoId = videoContainer.dataset.id;
-  if (text === "") {
-    return;
-  }
-  const response = await fetch(`/api/videos/${videoId}/comment`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ text }),
-  });
-  if (response.status === 201) {
-    textarea.value = "";
-    const { newCommentId } = await response.json();
-    addComment(text, newCommentId);
-  }
+    event.preventDefault();
+    const textarea = form.querySelector("textarea");
+    const text = textarea.value;
+    const videoId = videoContainer.dataset.id;
+    if (text === "") {
+        return;
+    }
+    const response = await fetch(`/api/videos/${videoId}/comment`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text }),
+    });
+    if (response.status === 201) {
+        textarea.value = "";
+        const { newCommentId } = await response.json();
+        addComment(text, newCommentId);
+    }
 };
 
 const handleDeleteComment = async (event) => {
-  const { target } = event;
-  const commentId = target.parentNode.parentNode.parentNode.dataset.id;
-  const response = await fetch(`/api/comments/${commentId}/delete`, {
-    method: "DELETE",
-  });
-  if (response.status === 201) {
-    deleteComment(target);
-  }
+    const { target } = event;
+    let commentId;
+    if (target.className === DELETE_BOX) {
+        commentId = target.parentNode.parentNode.parentNode.parentNode.dataset.id;
+    } else {
+        commentId = target.parentNode.parentNode.parentNode.parentNode.parentNode.dataset.id;
+    }
+    console.log(commentId);
+    const response = await fetch(`/api/comments/${commentId}/delete`, {
+        method: "DELETE",
+    });
+    if (response.status === 201) {
+        deleteComment(target);
+    }
 };
 
 const handleCommentMenuBtn = (event) => {
-  const { target } = event;
-  const menu = target.parentNode.nextSibling;
-  menu.style.display = "inline-block";
-  menu.style.position = "absolute";
-  menu.style.top =
-    target.getBoundingClientRect().y +
-    target.getBoundingClientRect().height +
-    pageYOffset +
-    "px";
-  menu.style.left = target.getBoundingClientRect().x + "px";
-  console.log(target.getBoundingClientRect());
-  console.log(target.getBoundingClientRect().y);
-  console.log(target.getBoundingClientRect().x);
-  console.log(pageYOffset);
+    window.removeEventListener("click", handleRemoveCommentMenu);
+    const { target } = event;
+    const menu = target.parentNode.nextSibling;
+    menu.style.display = "inline-block";
+    menu.style.position = "absolute";
+    menu.style.top = target.getBoundingClientRect().y + target.getBoundingClientRect().height + pageYOffset + "px";
+    menu.style.left = target.getBoundingClientRect().x + "px";
+    setTimeout(() => {
+        window.addEventListener("click", handleRemoveCommentMenu);
+    }, 300);
+};
+
+const handleRemoveCommentMenu = () => {
+    commentMenus.forEach((btn) => {
+        btn.style.display = "none";
+    });
+    if (commentMenuBtns) {
+        commentMenuBtns.forEach((btn) => btn.addEventListener("click", handleCommentMenuBtn));
+    }
 };
 
 const handleClickIcon = (event) => {
-  const { target } = event;
-  target.classList.add("click-after");
-  target.style.color = "cornflowerblue";
-  setTimeout(() => {
-    target.classList.remove("click-after");
-  }, 300);
+    const { target } = event;
+    const COMMENT_MENU = "comment__menu";
+    target.classList.add("click-after");
+    if (!target.classList.contains(COMMENT_MENU)) {
+        if (target.style.color == "" || target.style.color === "rgb(144, 144, 144)") {
+            target.style.color = "cornflowerblue";
+        } else {
+            target.style.color = "#909090";
+        }
+    }
+    if (target.classList.contains("comment__heart")) {
+        if (target.style.color === "cornflowerblue") {
+            target.className = "fas fa-heart comment__heart";
+            target.style.color = "#cc4646";
+        } else {
+            target.className = "far fa-heart comment__heart";
+            target.style.color = "#909090";
+        }
+    }
+    setTimeout(() => {
+        target.classList.remove("click-after");
+    }, 300);
 };
 
 if (form) {
-  form.addEventListener("submit", handleSubmit);
+    form.addEventListener("submit", handleSubmit);
 }
 
-if (deleteBtn) {
-  deleteBtn.forEach((btn) =>
-    btn.addEventListener("click", handleDeleteComment)
-  );
+if (deleteBtns) {
+    deleteBtns.forEach((btn) => btn.addEventListener("click", handleDeleteComment));
 }
 
-// likesBtn.forEach((btn) => btn.addEventListener("click", handleLikeComment));
-// dislikesBtn.forEach((btn) =>
+// likesBtns.forEach((btn) => btn.addEventListener("click", handleLikeComment));
+// dislikesBtns.forEach((btn) =>
 //   btn.addEventListener("click", handleDislikeComment)
 // );
 
-if (CommentMenuBtn) {
-  CommentMenuBtn.forEach((btn) =>
-    btn.addEventListener("click", handleCommentMenuBtn)
-  );
+if (commentMenuBtns) {
+    commentMenuBtns.forEach((btn) => btn.addEventListener("click", handleCommentMenuBtn));
 }
 
-document
-  .querySelectorAll(".comment__icons .fa-thumbs-up")
-  .forEach((btn) => btn.addEventListener("click", handleClickIcon));
-document
-  .querySelectorAll(".comment__icons .fa-thumbs-down")
-  .forEach((btn) => btn.addEventListener("click", handleClickIcon));
-document
-  .querySelectorAll(".comment__menu")
-  .forEach((btn) => btn.addEventListener("click", handleClickIcon));
+likesBtns.forEach((btn) => btn.addEventListener("click", handleClickIcon));
+dislikesBtns.forEach((btn) => btn.addEventListener("click", handleClickIcon));
+lovesBtns.forEach((btn) => btn.addEventListener("click", handleClickIcon));
+commentMenuBtns.forEach((btn) => btn.addEventListener("click", handleClickIcon));
--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -23,114 +23,111 @@ video.value = volumeValue;
 // console.log(play, mute, time, volume);
 
 const handlePlayClick = (e) => {
-  // if the video is playing, pause it
-  // else play the video.
-  if (video.paused) {
-    video.play();
-  } else {
-    video.pause();
-  }
-  playBtnIcon.classList = video.paused ? "fas fa-play" : "fas fa-pause";
+    // if the video is playing, pause it
+    // else play the video.
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+    playBtnIcon.classList = video.paused ? "fas fa-play" : "fas fa-pause";
 };
 
 const handleMute = (e) => {
-  video.muted = video.muted ? false : true;
-  muteBtnIcon.classList = video.muted
-    ? "fas fa-volume-mute"
-    : "fas fa-volume-up";
-  volumeRange.value = video.muted ? 0 : volumeValue;
+    video.muted = video.muted ? false : true;
+    muteBtnIcon.classList = video.muted ? "fas fa-volume-mute" : "fas fa-volume-up";
+    volumeRange.value = video.muted ? 0 : volumeValue;
 };
 
 const handleVolumeChange = (event) => {
-  const {
-    target: { value },
-  } = event;
-  if (video.muted) {
-    video.muted = false;
-    muteBtnIcon.classList = "fas fa-volume-up";
-  }
-  console.log(value);
-  volumeValue = value;
-  video.volume = value;
-  if (value == 0) {
-    video.muted = true;
-    muteBtnIcon.classList = "fas fa-volume-mute";
-    volumeValue = 0.5;
-  } else {
-    video.muted = false;
-    muteBtnIcon.classList = "fas fa-volume-up";
-  }
+    const {
+        target: { value },
+    } = event;
+    if (video.muted) {
+        video.muted = false;
+        muteBtnIcon.classList = "fas fa-volume-up";
+    }
+    console.log(value);
+    volumeValue = value;
+    video.volume = value;
+    if (value == 0) {
+        video.muted = true;
+        muteBtnIcon.classList = "fas fa-volume-mute";
+        volumeValue = 0.5;
+    } else {
+        video.muted = false;
+        muteBtnIcon.classList = "fas fa-volume-up";
+    }
 };
 
-const formatTime = (seconds) =>
-  new Date(seconds * 1000).toISOString().substr(14, 5);
+const formatTime = (seconds) => new Date(seconds * 1000).toISOString().substr(14, 5);
 
 const handleLoadedMetadata = () => {
-  totalTime.innerText = formatTime(Math.floor(video.duration));
-  timeline.max = Math.floor(video.duration);
+    totalTime.innerText = formatTime(Math.floor(video.duration));
+    timeline.max = Math.floor(video.duration);
 };
 
 const handleTimeUpdate = () => {
-  currentTime.innerText = formatTime(Math.floor(video.currentTime));
-  timeline.value = Math.floor(video.currentTime);
+    currentTime.innerText = formatTime(Math.floor(video.currentTime));
+    timeline.value = Math.floor(video.currentTime);
 };
 
 const handleEnded = () => {
-  const { id } = videoContainer.dataset;
-  playBtnIcon.classList = "fas fa-play";
-  fetch(`/api/videos/${id}/view`, { method: "POST" });
+    const { id } = videoContainer.dataset;
+    playBtnIcon.classList = "fas fa-play";
+    fetch(`/api/videos/${id}/view`, { method: "POST" });
 };
 
 const handleTimelineChange = (event) => {
-  const {
-    target: { value },
-  } = event;
-  video.currentTime = value;
+    const {
+        target: { value },
+    } = event;
+    video.currentTime = value;
 };
 
 const handleFullScreen = () => {
-  const fullScreen = document.fullscreenElement;
-  if (fullScreen) {
-    document.exitFullscreen();
-  } else {
-    videoContainer.requestFullscreen();
-  }
-  fullScreenIcon.classList = fullScreen ? "fas fa-expand" : "fas fa-compress";
+    const fullScreen = document.fullscreenElement;
+    if (fullScreen) {
+        document.exitFullscreen();
+    } else {
+        videoContainer.requestFullscreen();
+    }
+    fullScreenIcon.classList = fullScreen ? "fas fa-expand" : "fas fa-compress";
 };
 
 const hideControls = () => videoControls.classList.remove("showing");
 
 const handleMouseMove = () => {
-  if (controlsTimeout) {
-    clearTimeout(controlsTimeout);
-    controlsTimeout = null;
-  }
-  if (constrolsMouvementTimeout) {
-    clearTimeout(constrolsMouvementTimeout);
-    constrolsMouvementTimeout = null;
-  }
-  videoControls.classList.add("showing");
-  constrolsMouvementTimeout = setTimeout(hideControls, 3000);
+    if (controlsTimeout) {
+        clearTimeout(controlsTimeout);
+        controlsTimeout = null;
+    }
+    if (constrolsMouvementTimeout) {
+        clearTimeout(constrolsMouvementTimeout);
+        constrolsMouvementTimeout = null;
+    }
+    videoControls.classList.add("showing");
+    constrolsMouvementTimeout = setTimeout(hideControls, 3000);
 };
 
 const handleMouseLeave = () => {
-  controlsTimeout = setTimeout(hideControls, 3000);
+    controlsTimeout = setTimeout(hideControls, 3000);
 };
 
 const handleSpacebarDown = (event) => {
-  // event.keyCode = 32 window spacebar
-  if (event.code == "Space") {
-    event.preventDefault();
-    handlePlayClick();
-  }
+    // event.keyCode = 32 window spacebar
+    if (event.code == "Space") {
+        event.preventDefault();
+        handlePlayClick();
+    }
 };
 
 const handleStopSpacebarDown = () => {
-  window.removeEventListener("keydown", handleSpacebarDown);
+    window.removeEventListener("keydown", handleSpacebarDown);
 };
 
 const handleSpacebarDownAgain = () => {
-  window.addEventListener("keydown", handleSpacebarDown);
+    window.addEventListener("keydown", handleSpacebarDown);
 };
 
 playBtn.addEventListener("click", handlePlayClick);
@@ -146,6 +143,6 @@ timeline.addEventListener("input", handleTimelineChange);
 fullScreenBtn.addEventListener("click", handleFullScreen);
 window.addEventListener("keydown", handleSpacebarDown);
 if (commentForm) {
-  commentForm.addEventListener("click", handleStopSpacebarDown);
-  commentForm.addEventListener("focusout", handleSpacebarDownAgain);
+    commentForm.addEventListener("click", handleStopSpacebarDown);
+    commentForm.addEventListener("focusout", handleSpacebarDownAgain);
 }
