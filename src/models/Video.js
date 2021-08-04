368791d65bf78ddcdfdb97873b481140b9f6bee5
tlsgosk2120 <tlsgosksla@gmail.com>
2021-08-04 22:27:15 +0900

    ing

--- src/models/Video.js
+++ src/models/Video.js
@@ -9,6 +9,7 @@ const videoSchema = new mongoose.Schema({
     hashtags: [{ type: String, trim: true }],
     meta: {
         views: { type: Number, default: 0, required: true },
+        likes: { type: Number, default: 0 },
     },
     owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
